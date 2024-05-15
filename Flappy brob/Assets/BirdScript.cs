aa9cb5e7dd4bb1ad8b158f97dbc35313a76691d5
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2024-05-15 00:10:58 -0700

    Basic Finished floopy turb

--- Flappy brob/Assets/BirdScript.cs
+++ Flappy brob/Assets/BirdScript.cs
@@ -1,25 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+        // For testing purposes.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
